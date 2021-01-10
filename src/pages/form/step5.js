259cde50804834576b0af55e89bfa84263eaf97d
Azhari Subroto <juarathemes@gmail.com>
2021-01-10 15:14:21 +0700

    featurebox lineheight fix

--- src/pages/form/step5.js
+++ src/pages/form/step5.js
@@ -50,7 +50,8 @@ const formControlStyle = {
     }
   },
   '& fieldset': {
-    border: '1px solid #dfdfdf'
+    border: '1px solid #dfdfdf',
+    top: 0
   },
   '& .MuiSelect-select': {
     paddingTop: 0,
