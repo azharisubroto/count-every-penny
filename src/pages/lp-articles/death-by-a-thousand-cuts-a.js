edb368c399ac64d6e7040af40032a5a23d9d04df
Azhari Subroto <juarathemes@gmail.com>
2021-01-10 12:37:46 +0700

    responsive article components

--- src/pages/lp-articles/death-by-a-thousand-cuts-a.js
+++ src/pages/lp-articles/death-by-a-thousand-cuts-a.js
@@ -2,7 +2,16 @@ import DeathByThousandcuts from '@/components/Articles/DeathByThousandcuts'
 import Article from '@/layout/Article'
 
 export default function deathbythousandcutsA() {
-  return <DeathByThousandcuts />
+  return (
+    <>
+      <DeathByThousandcuts />
+      <style jsx global>{`
+        body {
+          background: #f3f3f3 !important;
+        }
+      `}</style>
+    </>
+  )
 }
 
 deathbythousandcutsA.Layout = Article
--- src/pages/lp-articles/death-by-a-thousand-cuts-b.js
+++ src/pages/lp-articles/death-by-a-thousand-cuts-b.js
@@ -2,7 +2,16 @@ import DeathByThousandcuts from '@/components/Articles/DeathByThousandcuts'
 import Article2 from '@/layout/Article2'
 
 export default function deathbythousandcutsA() {
-  return <DeathByThousandcuts />
+  return (
+    <>
+      <DeathByThousandcuts />
+      <style jsx global>{`
+        body {
+          background: #f3f3f3 !important;
+        }
+      `}</style>
+    </>
+  )
 }
 
 deathbythousandcutsA.Layout = Article2
