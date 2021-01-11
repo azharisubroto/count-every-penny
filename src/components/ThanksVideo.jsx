2c4d4c0b4cea6f1a1c3664803e8348538750a2aa
Teguh Rianto <teguh.rianto22@gmail.com>
2021-01-11 14:13:01 +0700

    fix logo & behaviour embed vid

--- src/components/ThanksVideo.jsx
+++ src/components/ThanksVideo.jsx
@@ -1,32 +1,17 @@
-import React from 'react'
-import Slide from '@material-ui/core/Slide'
-import Dialog from '@material-ui/core/Dialog'
-
-const Transition = React.forwardRef(function Transition(props, ref) {
-  return <Slide direction="up" ref={ref} {...props} />
-})
-
-function AdrewVideo() {
-  const [open, setOpen] = React.useState(false)
-  const yt_link = 'https://www.youtube.com/embed/7QltMeIbkaM'
-
-  const handleClickOpen = () => {
-    setOpen(true)
-  }
-
-  const handleClose = () => {
-    setOpen(false)
-  }
-
+/* eslint-disable jsx-a11y/iframe-has-title */
+function ThanksVideo() {
   return (
     <figure className="article-video mx-0">
       <div className="row g-0">
         <div className="col-lg-8">
           <div className="article-video__thumb">
-            <img src="/static/img/thankyou/thumb.png" alt="Health" />
-            <button onClick={handleClickOpen} className="article-video__play">
-              <img src="/static/img/thankyou/play.png" alt="Play" />
-            </button>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/7QltMeIbkaM"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen></iframe>
           </div>
         </div>
         <div className="col-lg-4 card mb-0">
@@ -39,25 +24,8 @@ function AdrewVideo() {
           </div>
         </div>
       </div>
-
-      <Dialog
-        open={open}
-        TransitionComponent={Transition}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-slide-title"
-        aria-describedby="alert-dialog-slide-description">
-        <iframe
-          title="video"
-          src={yt_link}
-          frameBorder="0"
-          autoPlay="1"
-          width="600"
-          height="400"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen></iframe>
-      </Dialog>
     </figure>
   )
 }
 
-export default AdrewVideo
+export default ThanksVideo
