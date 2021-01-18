c1c72843c94a2154659706f1b01df1ee0c9ed395
Azhari Subroto <juarathemes@gmail.com>
2021-01-18 13:53:45 +0700

    homepage redirect

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,21 @@
 //import Link from 'next/link'
+import React from 'react'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 export default function Home() {
+  const router = useRouter()
+
+  React.useEffect(() => {
+    router.push('/form/step1')
+  }, [])
+
   return (
     <>
       <Head>
         <title>Count Every Penny</title>
       </Head>
-      <article>Homepage</article>
+      <article></article>
     </>
   )
 }
